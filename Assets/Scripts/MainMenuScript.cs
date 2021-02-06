156ef51caf4d6bf5b271c04150a84e4fb616645c
JordynWinnie <jordynwinnie@gmail.com>
2021-02-06 12:38:57 +0800

    Added Name Input

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,26 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour
 {
-    [SerializeField] private GameObject mainMenu;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject[] menuObjects;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        //DisplayMenu(0);
     }
 
     public void QuitGame() => Application.Quit();
-
-    public void ShowMenu()
+    
+    public void DisplayMenu(int displayId)
     {
+        foreach (var menuObject in menuObjects)
+        {
+            menuObject.SetActive(false);
+        }
         
+        menuObjects[displayId].SetActive(true);
     }
 }
