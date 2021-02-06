1f0ab35e824a4aa29bd9ce09c7c7738bdb78204a
JordynWinnie <jordynwinnie@gmail.com>
2021-02-06 17:46:10 +0800

    MainScreen implemented

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
     [SerializeField] private GameObject[] menuObjects;
+    [SerializeField] private TMP_InputField nameInput;
+    [SerializeField] private Button playGameButton;
+    [SerializeField] private Toggle femaleToggle;
 
     private void Start()
     {
@@ -22,5 +27,22 @@ public class MainMenuScript : MonoBehaviour
         }
         
         menuObjects[displayId].SetActive(true);
+    }
+
+    public void SetName()
+    {
+        if (nameInput.text.Equals(string.Empty))
+        {
+            playGameButton.interactable = false;
+        }
+        else
+        {
+            playGameButton.interactable = true;
+        }
+    }
+
+    public void StartGame()
+    {
+        
     }
 }
