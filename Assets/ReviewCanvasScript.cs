a80800e3c89ec70b5b446ea396c2f9cb4d718ce5
JordynWinnie <jordynwinnie@gmail.com>
2021-02-12 00:27:11 +0800

    Added Reviews

---
+++ Assets/ReviewCanvasScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReviewCanvasScript : MonoBehaviour
+{
+    [SerializeField] private Button nextButton;
+
+    [SerializeField] private TextMeshProUGUI review1Text;
+    [SerializeField] private TextMeshProUGUI reviewCompany1Text;
+    [SerializeField] private TextMeshProUGUI review2Text;
+    [SerializeField] private TextMeshProUGUI reviewCompany2Text;
+    [SerializeField] private TextMeshProUGUI review3Text;
+    [SerializeField] private TextMeshProUGUI reviewCompany3Text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseHUD()
+    {
+        Destroy(gameObject);
+    }
+}
