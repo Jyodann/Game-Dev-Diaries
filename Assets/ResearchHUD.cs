861920ec72f861f2867a61a8cee24430392a5a85
Jordyn <48559311+JordynWinnie@users.noreply.github.com>
2021-02-12 16:48:16 +0800

    Research Canvas added

---
+++ Assets/ResearchHUD.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchHUD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseHUD() => Destroy(gameObject);
+
+    public void OpenTopicChooser()
+    {
+        ChooserMenu.Instance.OpenMenuWithInformation("Choose Topic", GameDynamicData.Instance.CurrentUserTopics);
+        ChooserMenu.OnButtonFinish += OnTopicChoose;
+    }
+    
+    public void OpenGenreChooser()
+    {
+        ChooserMenu.Instance.OpenMenuWithInformation("Choose genre to research on", GameDynamicData.Instance.CurrentGenres);
+    }
+
+    private void OnTopicChoose(string topic)
+    {
+        
+    }
+}
